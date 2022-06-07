28ace44913f10cba86ca7cea0af4b8a59c743979
Stefano Befaro <stefanobefaro@gmail.com>
2022-06-06 22:01:18 -0300

    Created dynamic product detail views

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,21 @@
 const path = require('path')
+const fs = require('fs');
+const dbPath = path.join(__dirname,'../database')
+
+const readJSON = (path) => {
+    file = fs.readFileSync(path,'utf-8');
+    fileParsed = JSON.parse(file);
+    return fileParsed;
+}
+
 const controller = {
     productCart: (req,res) => res.render('products/productCart'),
-    productDetail: (req,res) => res.render('products/productDetail'),
+    productDetail: (req,res) => {
+        const id = req.params.id;
+        const productos = readJSON(path.join(dbPath,'productos.json'))
+        const productoDetallado = productos.find((producto) => producto.id == id);
+        res.render('products/productDetail', {producto: productoDetallado})
+    },
 }
 
 
--- src/routers/productsRoutes.js
+++ src/routers/productsRoutes.js
@@ -4,6 +4,7 @@ const productsController = require("../controllers/productsController")
 
 router.get("/productCart", productsController.productCart)
 router.get("/productDetail", productsController.productDetail)
+router.get("/productDetail/:id", productsController.productDetail)
 
 
 module.exports = router
