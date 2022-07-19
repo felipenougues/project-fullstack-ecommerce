d205866e894ed4c9565488c5e882cf8e868fa685
felipenougues <felipenougues1@gmail.com>
2022-07-18 22:16:44 -0400

    nuevo Multer para guardar img usuarios

--- src/routers/usersRoutes.js
+++ src/routers/usersRoutes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
-const upload = require('../middlewares/multer');
 const path = require('path');
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "../public/images/users"))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+    }
+  });
+  
+  const upload = multer({ storage: storage });
+
+  module.exports = upload;
+  
+
+
+  
+
 
 const { body } = require('express-validator');
 
