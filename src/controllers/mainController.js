de4553dcdc8c41ac858e1f4239ba052e2f23c7f3
Elynelez <Elinarnyex@gmail.com>
2022-08-19 17:02:21 -0500

    diagrama bases de datos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const fs = require('fs');
+const db = require('../database/models');
 const dbPath = path.join(__dirname,'../database')
 const productos = JSON.parse(fs.readFileSync(path.join(dbPath,'products','products.json'),'utf-8'))
 const usuarios = JSON.parse(fs.readFileSync(path.join(dbPath,'users','users.json'),'utf-8'))
 
+
+
+
 const controller = {
     index: (req,res) => {
-
         res.render('index', {productos: productos, usuarios:usuarios})
-
-
     },
       
 }
-
 
 module.exports = controller
