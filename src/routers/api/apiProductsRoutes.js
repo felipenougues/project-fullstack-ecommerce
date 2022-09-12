ae03f2722fe9a2b27bc08007ff1db8aebc322549
Elynelez <Elinarnyex@gmail.com>
2022-09-12 11:25:51 -0500

    pequeñas notas en api routes

--- src/routers/api/apiProductsRoutes.js
+++ src/routers/api/apiProductsRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router();
 const apiProductsController = require('../../controllers/api/apiProductsController')
 
+
+// consultar como http://localhost:3030/api/products/
 router.get("/", apiProductsController.list)
+// consultar como http://localhost:3030/api/products/{id}
 router.get("/:id", apiProductsController.detail)
 
 module.exports = router
--- src/routers/api/apiUsersRoutes.js
+++ src/routers/api/apiUsersRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router();
 const apiUsersController = require('../../controllers/api/apiUsersController')
 
+
+// consultar como http://localhost:3030/api/users/
 router.get('/', apiUsersController.list)
+// consultar como http://localhost:3030/api/users/{id}
 router.get('/:id', apiUsersController.detail)
 
 module.exports = router
