ddf7c8baed668e6bafe5568d19b9cabbfdd4bb77
Stefano Befaro <stefanobefaro@gmail.com>
2022-09-19 19:52:41 -0300

    Fixed contoller

--- src/controllers/api/apiProductsController.js
+++ src/controllers/api/apiProductsController.js
@@ -1,12 +1,10 @@
 const db = require('../../database/models')
-const { sequelize } = require('sequelize')
 module.exports = {
 
     list: (req,res) => {
         db.Product.findAll({
             include: [{association: 'category'}],
             attributes: ["id","name","description","category_id","price", "image"]
-        /* [sequelize.fn('concat', "la url", "/detalle", sequelize.col("product_id")), "alias"] */
         }) 
         .then(products => {
             let productsDetails = products.map(product => {
