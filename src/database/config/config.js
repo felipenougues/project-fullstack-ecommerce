1586fcc772526aade6f0e869ee0c3483655d9aa1
Stefano Befaro <stefanobefaro@gmail.com>
2022-10-11 16:38:46 -0300

    Created production database for deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b02041c040e885",
+    "password": "347e5acf",
+    "database": "heroku_e0abd2ac8c1846c",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   }
 }
